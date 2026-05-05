b704a88470bc5c388fef43bd8f3cd3ecbf4f3d62
leo2852.info@gmail.com <llp@MacBook-Air-di-Leonardo.local>
2026-05-05 14:35:53 +0200

    minor changes

--- NA3 code/regulation_script.m
+++ NA3 code/regulation_script.m
@@ -8,34 +8,43 @@ addpath(genpath(fullfile(pwd,'..','utils')));
 %% Set simulation parameters
 r_nominal = 0.03;
 d_nominal = 0.165;
-r = r_nominal;
-d = d_nominal;
-r_actual = r_nominal;
-d_actual = d_nominal;
-% r_actual = 0.0302;
-% d_actual = 0.1694;
-omega_M = 12;
+%r = r_nominal;
+%d = d_nominal;
+%r_actual = r_nominal;
+%d_actual = d_nominal;
+ r_actual = 0.0302;
+ d_actual = 0.1694;
+ r = r_actual;
+d = d_actual;
+omega_M = 10;
 
+tol = 0.000001;
 
-controller_index = 1; % 1->cartesian, 2->posture
+controller_index = 2; % 1->cartesian, 2->posture
 flg_replanning = true;
 % desired configuration
-q_d = [2;2;0];
+q_d = [0;0;0];
 % initial configuration
-Q_INIT = [1;1;0];
+Q_INIT = [0;-2;pi/2+0.01];
 % simulation time
-T_SIM = 20;
+T_SIM = 60;
 
 %% Set controller parameters
 if controller_index == 1
     % cartesian
-    k_1 = 10; 
-    k_2 = 10;
+    k_1 = 0.5; 
+    k_2 = 1;
     control_par = [k_1, k_2, 0];
 else
     % posture
-    k_1 = 1; 
-    k_2 = 1;
-    k_3 = 10;
+    %k_1 =0.80; 
+    %k_2 = 0.75;
+    %k_3 = 0.3;
+    k_1 = 0.8;
+    k_2 = 0.75;
+    k_3 = 0.3;
     control_par = [k_1, k_2, k_3];
 end
+
+%%
+sim("sim_regulation.slx");
